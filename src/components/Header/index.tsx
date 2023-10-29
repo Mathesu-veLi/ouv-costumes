44d89bfc9a1748292f3c02a3d18c93fd2d2fae98
Mathesu-veLi <matheuslevit@gmail.com>
2023-10-29 08:23:49 -0300

    Fix responsive menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,19 +5,44 @@ import { Nav, Navbar } from 'react-bootstrap';
 
 export default function Header() {
     return (
-        <Navbar expand="lg" className="navbar">
-            <Navbar.Brand className="position-absolute">
-                <img src={ouvLogo} alt="" width={180} className='mx-5'/>
-            </Navbar.Brand>
+        <header>
+            <Navbar expand="md" className="navbar py-4">
+                <Navbar.Brand className="position-absolute fixed-top mx-5 h-100">
+                    <img src={ouvLogo} alt="" width={120} />
+                </Navbar.Brand>
 
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto mx-auto nav">
-                    <Nav.Link className="px-5" href='/'>Início</Nav.Link>
-                    <Nav.Link className="px-5" href='shop'>Comprar</Nav.Link>
-                    <Nav.Link className="px-5" href='contact'>Contato</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+                <Navbar.Toggle
+                    className="ms-auto"
+                    data-mdb-target="basic-nav"
+                    aria-controls="basic-nav"
+                    aria-expanded="false"
+                    data-mdb-toggle="collapse"
+                />
+                <Navbar.Collapse id="basic-nav">
+                    <Nav className="me-auto mx-auto nav pt-2">
+                        <Nav.Link
+                            className="px-5 m-0 btn-block text-center "
+                            href="/"
+                        >
+                            Início
+                        </Nav.Link>
+
+                        <Nav.Link
+                            className="px-5 m-0 btn-block text-center "
+                            href="shop"
+                        >
+                            Comprar
+                        </Nav.Link>
+
+                        <Nav.Link
+                            className="px-5 m-0 btn-block text-center "
+                            href="contact"
+                        >
+                            Contato
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </header>
     );
 }

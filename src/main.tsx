c8d10cbfca95d36b6f8493587afc5cea4177cc99
Mathesu-veLi <matheuslevit@gmail.com>
2023-10-29 08:28:14 -0300

    Add login route

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/index.tsx';
+import Login from './pages/Login/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <Header />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>,
 );
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "react-bootstrap";
+
+export default function Login(){
+    return (
+        <Container fluid>
+            <h1>Login</h1>
+        </Container>
+    )
+}

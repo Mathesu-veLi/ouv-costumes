ce1396c12d49c562b7830a3f8d88b8530f3d9fdb
Mathesu-veLi <matheuslevit@gmail.com>
2023-10-29 10:10:46 -0300

    Create login page and configurate site's colors

---
+++ src/config/colors.ts
@@ -0,0 +1,3 @@
+export const primaryColor = '#21333f';
+export const primaryDarkColor = '#01131f';
+export const primaryLightColor = '#91a3af';
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,34 @@
-import { Container } from "react-bootstrap";
+import { Container } from 'react-bootstrap';
+import { Form } from './styled.ts';
+import './style.css';
 
-export default function Login(){
+export default function Login() {
     return (
-        <Container fluid>
-            <h1>Login</h1>
+        <Container
+            fluid
+            className="d-flex vh-100 justify-content-center flex-column align-items-center"
+        >
+            <div>
+                <Form method="post" className="d-flex flex-column">
+                    <h1 className="text-center">Login</h1>
+                    <label htmlFor="email">E-mail:</label>
+                    <input
+                        type="email"
+                        name="password"
+                        id="email"
+                        placeholder="exemple@exemple.com"
+                    />
+                    <label htmlFor="password" className="mt-3">
+                        Password:
+                    </label>
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="password123"
+                    />
+                </Form>
+            </div>
         </Container>
-    )
+    );
 }

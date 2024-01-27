544bcb54ff0441deb1cac268fda9c705ca97fb9b
Matheus Levi <matheuslevit@gmail.com>
2024-01-27 16:23:54 -0300

    rename file

--- server/src/modules/users/users.service.ts
+++ server/src/modules/users/users.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { userNotExistThrow } from '../../utils/userNotExistThrow';
+import { userNotExistThrow } from '../../utils/throws';
 import { generatePasswordHash } from '../../utils/passwordUtils';
 
 @Injectable()

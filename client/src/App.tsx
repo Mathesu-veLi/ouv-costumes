f35062ab78a0aae8225465e538f7e811aadfa51e
Matheus Levi <matheuslevit@gmail.com>
2024-01-28 01:30:37 -0300

    change routes management

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,34 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './containers/Home';
 import { Header } from './components/Header';
 import { Login } from './containers/Login';
+import { Register } from './containers/Register';
 
 function App() {
+  const routes = [
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/login',
+      element: <Login />,
+    },
+  ];
+  
   return (
     <>
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          {routes.map((route) => {
+            return (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={route.element}
+              />
+            );
+          })}
         </Routes>
       </BrowserRouter>
     </>

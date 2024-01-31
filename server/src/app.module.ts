1eb72a60425da69286386d910545ede2c0c3aba3
Matheus <matheuslevit@gmail.com>
2024-01-31 16:00:30 -0300

    allows update and delete operations only if the token is valid

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,28 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { TokenModule } from './modules/token/token.module';
+import { LoginRequiredMiddleware } from './middlewares/loginRequired';
 
 @Module({
   imports: [UsersModule, PrismaModule, TokenModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoginRequiredMiddleware)
+      .forRoutes(
+        { path: 'users/:id', method: RequestMethod.PATCH },
+        { path: 'users/:id', method: RequestMethod.DELETE },
+      );
+  }
+}

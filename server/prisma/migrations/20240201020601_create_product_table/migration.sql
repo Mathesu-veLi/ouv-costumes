3cd8a728c469c6d65723bdb560ba5ace230b69e0
Matheus <matheuslevit@gmail.com>
2024-01-31 23:06:29 -0300

    transform product name and product image in uniques

--- server/prisma/migrations/20240201020601_create_product_table/migration.sql
+++ server/prisma/migrations/20240201020601_create_product_table/migration.sql
@@ -4,9 +4,14 @@ CREATE TABLE "Product" (
     "name" TEXT NOT NULL,
     "img" TEXT NOT NULL,
     "price" DOUBLE PRECISION NOT NULL,
-    "description" TEXT NOT NULL,
     "stock" INTEGER NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_name_key" ON "Product"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_img_key" ON "Product"("img");

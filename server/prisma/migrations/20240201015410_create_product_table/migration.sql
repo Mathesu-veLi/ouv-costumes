c68fef2e547eede314fd746b3383815cb37fdeba
Matheus <matheuslevit@gmail.com>
2024-01-31 22:56:44 -0300

    change migration

--- server/prisma/migrations/20240201015410_create_product_table/migration.sql
+++ server/prisma/migrations/20240201015410_create_product_table/migration.sql
@@ -5,7 +5,8 @@ CREATE TABLE "Product" (
     "img" TEXT NOT NULL,
     "price" DOUBLE PRECISION NOT NULL,
     "description" TEXT NOT NULL,
-    "category" TEXT NOT NULL,
+    "stock" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
 );

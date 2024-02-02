e306fd600ef1e129e2b2f3e3e5ea0d9829aee66f
Matheus <matheuslevit@gmail.com>
2024-02-01 21:29:23 -0300

    add a responsive design to desktops

--- client/src/components/Product.tsx
+++ client/src/components/Product.tsx
@@ -9,11 +9,20 @@ export function Product(props: IProduct) {
   return (
     <Link to={`/product/${props.id}`}>
       <div className="rounded-lg shadow-lg flex flex-col justify-center items-center px-2 py-5 border flex-1 flex-grow my-4">
-        <img src={props.img} alt="banner" className="w-36 h-36 rounded-sm" />
+        <img
+          src={props.img}
+          alt="banner"
+          className="w-36 h-36 lg:w-52 lg:h-52 rounded-sm"
+        />
         <div className="pt-3 text-center flex flex-col gap-2 justify-center">
-          <h2 className="text-md max-w-32 min-h-12 flex justify-center items-center">
-            {props.name.length > 26
+          <h2 className="text-md lg:hidden max-w-32 min-h-12 flex justify-center items-center">
+            {props.name.length > 25
               ? `${props.name.substring(0, 26)}...`
+              : props.name}
+          </h2>
+          <h2 className="max-[1000px]:hidden lg:block text-md max-w-40 min-h-12 flex justify-center items-center">
+            {props.name.length > 40
+              ? `${props.name.substring(0, 41)}...`
               : props.name}
           </h2>
           <p className="text-sm text-gray-400">
--- client/src/containers/Products.tsx
+++ client/src/containers/Products.tsx
@@ -8,11 +8,7 @@ export function Products() {
 
   useEffect(() => {
     async function getProduct() {
-      const products = await api.get('products', {
-        data: {
-          
-        }
-      }).then((response) => {
+      const products = await api.get('products').then((response) => {
         return response.data;
       });
 
@@ -26,9 +22,9 @@ export function Products() {
   }, []);
 
   return (
-    <div className="pt-32">
-      <h1 className="text-center text-xl">Products</h1>
-      <div className="flex justify-around items-start mt-10 flex-wrap">
+    <div className="pt-32 lg:pt-40 lg:flex lg:justify-center lg:items-center lg:flex-col">
+      <h1 className="text-center text-xl lg:text-2xl">Products</h1>
+      <div className="flex justify-around lg:justify-center items-start mt-10 flex-wrap lg:max-w-6xl lg:gap-10">
         {products.map((product) => {
           return (
             <Product

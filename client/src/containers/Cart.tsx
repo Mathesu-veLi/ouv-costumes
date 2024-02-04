7d3bdc96971b9333c93aeda920d4630eb6cafee6
Matheus <matheuslevit@gmail.com>
2024-02-04 02:03:17 -0300

    fix responsive design to desktops in the cart page

--- client/src/containers/Cart.tsx
+++ client/src/containers/Cart.tsx
@@ -22,73 +22,73 @@ export function Cart() {
     );
 
   return (
-    <div className="flex flex-col lg:flex-row w-full justify-center items-center h-screen gap-5">
-      <div className="mx-3 w-[340px] flex flex-col gap-5">
-        <div className="w-full p-3 border">
-          <h1>My Cart</h1>
-        </div>
-
-        <div className="px-3 flex flex-col gap-4">
-          {products.map((product) => {
-            return (
-              <div key={product.id} className="flex gap-4 w-80">
-                <img
-                  src={`${API_URL}/uploads/${product.img}`}
-                  alt={product.name}
-                  className="w-36 border rounded-sm"
-                />
-
-                <div className="flex flex-col justify-between w-full">
-                  <h1>{product.name}</h1>
-                  <div className="flex flex-col gap-2 lg:flex-row justify-between">
-                    <div className="flex gap-4 justify-end items-center">
-                      <FaRegTrashCan title="Delete" />
-
-                      <div className="flex gap-2 justify-center items-center w-16">
-                        <FiMinusCircle
-                          onClick={() => decrementQuantity(product.id)}
-                        />
-                        <span>{product.quantity}</span>
-                        <FiPlusCircle
-                          onClick={() => incrementQuantity(product.id)}
-                        />
+    <div className="flex w-full justify-center items-center h-screen pt-80 lg:p-0">
+      <div className="flex flex-col lg:flex-row gap-14 justify-center lg:justify-around items-center pb-10 lg:p-0 lg:w-11/12">
+        <div className="mx-3 w-[340px] lg:w-full flex flex-col gap-5">
+          <div className="w-full p-3 border">
+            <h1>My Cart</h1>
+          </div>
+          <div className="px-3 flex flex-col gap-4 lg:gap-6">
+            {products.map((product) => {
+              return (
+                <div
+                  key={product.id}
+                  className="flex gap-4 lg:gap-6 w-80 lg:w-auto"
+                >
+                  <img
+                    src={`${API_URL}/uploads/${product.img}`}
+                    alt={product.name}
+                    className="w-36 lg:w-48 border rounded-sm"
+                  />
+                  <div className="flex flex-col justify-between w-full">
+                    <h1>{product.name}</h1>
+                    <div className="flex flex-col gap-2 lg:flex-row-reverse justify-between">
+                      <div className="flex gap-4 justify-end items-center">
+                        <FaRegTrashCan title="Delete" />
+                        <div className="flex gap-2 justify-center items-center w-16">
+                          <FiMinusCircle
+                            onClick={() => decrementQuantity(product.id)}
+                          />
+                          <span>{product.quantity}</span>
+                          <FiPlusCircle
+                            onClick={() => incrementQuantity(product.id)}
+                          />
+                        </div>
                       </div>
+                      <h1 className="font-bold">
+                        {formatToPrice(product.price)}
+                      </h1>
                     </div>
-
-                    <h1 className="font-bold">
-                      {formatToPrice(product.price)}
-                    </h1>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
-      </div>
-      <div className="border rounded-sm flex flex-col p-4 gap-5 mx-1 w-[340px]">
-        <p>Order Summary</p>
-        <div>
-          {products.map((product) => {
-            return (
-              <div
-                key={product.id}
-                className="flex justify-between gap-3 text-gray-300"
-              >
-                <div className="flex gap-3">
-                  <span>x{product.quantity}</span>
-                  <span>{product.name}</span>
+        <div className="border rounded-sm flex flex-col p-4 lg:p-6 gap-5 mx-1 w-[340px] lg:w-3/6">
+          <p>Order Summary</p>
+          <div>
+            {products.map((product) => {
+              return (
+                <div
+                  key={product.id}
+                  className="flex justify-between gap-3 text-gray-300"
+                >
+                  <div className="flex gap-3">
+                    <span>x{product.quantity}</span>
+                    <span>{product.name}</span>
+                  </div>
+                  <p className="flex justify-self-end">
+                    {formatToPrice(product.subTotal)}
+                  </p>
                 </div>
-                <p className="flex justify-self-end">
-                  {formatToPrice(product.subTotal)}
-                </p>
-              </div>
-            );
-          })}
-        </div>
-
-        <div className="flex justify-between py-2 border-t w-full">
-          <p>Order Total</p>
-          <p>{formatToPrice(totalPrice)}</p>
+              );
+            })}
+          </div>
+          <div className="flex justify-between py-2 border-t w-full">
+            <p>Order Total</p>
+            <p>{formatToPrice(totalPrice)}</p>
+          </div>
         </div>
       </div>
     </div>

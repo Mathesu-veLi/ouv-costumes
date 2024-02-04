e4e695ed4c39a2822b972dd606f27360c3272de0
Matheus <matheuslevit@gmail.com>
2024-02-04 01:30:50 -0300

    show the prodcuts in the cart

--- client/src/containers/Cart.tsx
+++ client/src/containers/Cart.tsx
@@ -1,10 +1,15 @@
 import { Button } from '@/components/ui/button';
 import { useCartStore } from '@/store/useCartStore';
 import { formatToPrice } from '@/utils/formatToPrice';
+import { API_URL } from '@/utils/globals';
 import { Link } from 'react-router-dom';
+import { FaRegTrashCan } from 'react-icons/fa6';
+import { FiPlusCircle } from 'react-icons/fi';
+import { FiMinusCircle } from 'react-icons/fi';
 
 export function Cart() {
-  const { products, totalPrice } = useCartStore();
+  const { products, totalPrice, incrementQuantity, decrementQuantity } =
+    useCartStore();
 
   if (!products.length)
     return (
@@ -17,9 +22,50 @@ export function Cart() {
     );
 
   return (
-    <div className="flex flex-col lg:flex-row w-full justify-center items-center h-screen">
-      <div></div>
-      <div className="border rounded-sm flex flex-col mx-1 p-4 gap-5">
+    <div className="flex flex-col lg:flex-row w-full justify-center items-center h-screen gap-5">
+      <div className="mx-3 w-[340px] flex flex-col gap-5">
+        <div className="w-full p-3 border">
+          <h1>My Cart</h1>
+        </div>
+
+        <div className="px-3">
+          {products.map((product) => {
+            return (
+              <div key={product.id} className="flex gap-4">
+                <img
+                  src={`${API_URL}/uploads/${product.img}`}
+                  alt={product.name}
+                  className="w-36 border rounded-sm"
+                />
+
+                <div className="flex flex-col justify-between">
+                  <h1>{product.name}</h1>
+                  <div className="flex flex-col gap-2 lg:flex-row justify-between">
+                    <div className="flex gap-4 justify-end items-center">
+                      <FaRegTrashCan title="Delete" />
+
+                      <div className="flex gap-2 justify-center items-center">
+                        <FiMinusCircle
+                          onClick={() => decrementQuantity(product.id)}
+                        />
+                        <span>{product.quantity}</span>
+                        <FiPlusCircle
+                          onClick={() => incrementQuantity(product.id)}
+                        />
+                      </div>
+                    </div>
+
+                    <h1 className="font-bold">
+                      {formatToPrice(product.price)}
+                    </h1>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="border rounded-sm flex flex-col p-4 gap-5 mx-1 w-[340px]">
         <p>Order Summary</p>
         <div>
           {products.map((product) => {

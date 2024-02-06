ee2d5c5cc1814598feda68588a41e592b5cf7767
Matheus <matheuslevit@gmail.com>
2024-02-06 19:23:28 -0300

    change require to import

--- server/src/modules/checkouts/checkouts.service.ts
+++ server/src/modules/checkouts/checkouts.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCheckoutDto } from './dto/create-checkout.dto';
+import Stripe from 'stripe';
 
 @Injectable()
 export class CheckoutsService {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  private stripe = require('stripe')(process.env.STRAPI_KEY);
+  private stripe = new Stripe(process.env.STRAPI_KEY);
   async create(createCheckoutDto: CreateCheckoutDto) {
     const session = await this.stripe.checkout.sessions.create({
       line_items: createCheckoutDto.lineItems,

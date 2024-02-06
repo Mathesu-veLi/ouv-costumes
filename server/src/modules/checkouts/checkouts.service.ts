462408e80e3c93c38fe6befc6e295bbd0c59cc4b
Matheus <matheuslevit@gmail.com>
2024-02-06 14:29:01 -0300

    creates a checkout service to create a link to the checkout session

---
+++ server/src/modules/checkouts/checkouts.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCheckoutDto } from './dto/create-checkout.dto';
+
+@Injectable()
+export class CheckoutsService {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  private stripe = require('stripe')(process.env.STRAPI_KEY);
+  async create(createCheckoutDto: CreateCheckoutDto) {
+    const session = await this.stripe.checkout.sessions.create({
+      line_items: createCheckoutDto.lineItems,
+      mode: 'payment',
+      success_url: `${process.env.CLIENT_URL}?success=true`,
+      cancel_url: `${process.env.CLIENT_URL}?canceled=true`,
+    });
+
+    return session.url;
+  }
+}

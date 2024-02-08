882e4550d8b0c9e6e1aa9a8451b7aa839e17484d
Matheus <matheuslevit@gmail.com>
2024-02-07 23:50:50 -0300

    fix server cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,8 +6,7 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    allowedHeaders: ['content-type'],
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: process.env.CLIENT_URL || 'http://192.168.100.3:5173',
     credentials: true,
   });
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {

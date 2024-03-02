b21123309ae5a68187ec402550b53d80849cf823
Matheus Levi <matheuslevit@gmail.com>
2024-03-02 15:05:20 -0300

    creates a loading component

---
+++ client/src/components/Loading.tsx
@@ -0,0 +1,7 @@
+export function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-2xl font-light">Loading...</h1>
+    </div>
+  );
+}

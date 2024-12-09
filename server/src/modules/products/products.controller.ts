97acb5de8a306d4a45cfccdf685ef358a74bfd5d
Matheus <matheuslevit@gmail.com>
2024-12-09 17:59:31 -0300

    restricts create, delete and update on the product endpoint to admins

--- server/src/modules/products/products.controller.ts
+++ server/src/modules/products/products.controller.ts
@@ -1,13 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Roles } from '@/decorators/roles.decorators';
+import { Role } from '../users/enum/user-type.enum';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @Roles(Role.Admin)
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
@@ -23,11 +34,13 @@ export class ProductsController {
   }
 
   @Patch(':id')
+  @Roles(Role.Admin)
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(+id, updateProductDto);
   }
 
   @Delete(':id')
+  @Roles(Role.Admin)
   remove(@Param('id') id: string) {
     return this.productsService.remove(+id);
   }

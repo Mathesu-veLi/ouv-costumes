af1b72c3ce334d2bf5e2754e6975fbfe8190861b
Matheus <matheuslevit@gmail.com>
2024-12-12 17:48:38 -0300

    validating role

--- server/src/guards/roles.guard.ts
+++ server/src/guards/roles.guard.ts
@@ -1,13 +1,18 @@
 import { ROLES_KEY } from '@/decorators/roles.decorators';
-import { Role } from '@/modules/users/enum/user-type.enum';
+import { LoginPayload } from '@/modules/token/dto/loginPayload.dto';
+import { Role } from '@prisma/client';
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  constructor(
+    private reflector: Reflector,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  canActivate(context: ExecutionContext): boolean {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
       context.getHandler(),
       context.getClass(),
@@ -15,7 +20,15 @@ export class RolesGuard implements CanActivate {
     if (!requiredRoles) {
       return true;
     }
-    const { user } = context.switchToHttp().getRequest();
-    return requiredRoles.some((role) => user.roles?.includes(role));
+
+    const { authorization } = context.switchToHttp().getRequest().headers;
+    const token = authorization.slice(7, authorization.length);
+
+    const loginPayload: LoginPayload | undefined = await this.jwtService
+      .verifyAsync(token, { secret: process.env.TOKEN_SECRET })
+      .catch(() => undefined);
+    if (!loginPayload) return false;
+
+    return requiredRoles.some((role) => role === loginPayload.role);
   }
 }

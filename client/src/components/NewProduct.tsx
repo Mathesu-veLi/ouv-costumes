e7f5d45b370bfe70ff567b9b506d6d74798bd8e3
Matheus <matheuslevit@gmail.com>
2025-01-01 19:33:34 -0300

    creates the form to add the product

--- client/src/components/NewProduct.tsx
+++ client/src/components/NewProduct.tsx
@@ -1,3 +1,4 @@
+import { useForm } from 'react-hook-form';
 import { Button } from './ui/button';
 import {
   DialogClose,
@@ -6,8 +7,52 @@ import {
   DialogHeader,
   DialogTitle,
 } from './ui/dialog';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { Input } from './ui/input';
+import { InputImg } from './InputImg';
+
+function checkFileType(file: File) {
+  const validMimeTypes = ['image/jpeg', 'image/png', 'image/jpg'];
+  return validMimeTypes.includes(file.type);
+}
+
+const formSchema = z.object({
+  image: z
+    .instanceof(File, { message: 'Product image must be specified' })
+    .refine(
+      (image) => checkFileType(image),
+      'Only image files (jpeg, png and jpg) are allowed',
+    ),
+  name: z.string().min(1),
+  price: z.number().min(1),
+  stock: z.number().min(1),
+});
+
+type TFormSchema = z.infer<typeof formSchema>;
 
 export function NewProduct() {
+  const form = useForm<TFormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      price: 1,
+      stock: 1,
+    },
+  });
+
+  async function addProduct(productForm: TFormSchema) {
+    console.log(productForm);
+  }
+
   return (
     <>
       <DialogHeader>
@@ -16,10 +61,82 @@ export function NewProduct() {
           Insert the information of the product
         </DialogDescription>
 
-        <div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(addProduct)}>
+            <div className="flex justify-around items-center gap-10 mb-4">
+              <div>
+                <FormField
+                  control={form.control}
+                  name="image"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Image</FormLabel>
+                      <FormControl>
+                        <InputImg field={field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="w-full">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="T-shirt" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="price"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Price</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="100"
+                          type="number"
+                          min={1}
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="stock"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Stock</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="12"
+                          type="number"
+                          min={1}
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+            <Button type="submit" className="w-full">
+              Add
+            </Button>
+          </form>
+        </Form>
 
-        </div>
-        
         <DialogFooter className="sm:justify-start">
           <DialogClose asChild>
             <Button type="button" variant="secondary">

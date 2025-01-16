1ee639d20deb03c2fd4c64820d7c3ff63f0f8bb2
Matheus <matheuslevit@gmail.com>
2025-01-16 16:16:07 -0300

    creates a endpoint to upload images

---
+++ server/src/modules/upload/upload.controller.ts
@@ -0,0 +1,31 @@
+import {
+  Controller,
+  FileTypeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('upload')
+export class UploadController {
+  @Post()
+  @UseInterceptors(FileInterceptor('image'))
+  uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [new FileTypeValidator({ fileType: 'image/*' })],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    if (!file) {
+      throw new Error('No file provided.');
+    }
+
+    console.log(file);
+
+    return { message: 'File uploaded successfully' };
+  }
+}

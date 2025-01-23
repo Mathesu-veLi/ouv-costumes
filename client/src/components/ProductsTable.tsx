8c9939e421aeab122d1cffc1eb679dfe746f1fe8
Matheus <matheuslevit@gmail.com>
2025-01-22 21:06:55 -0300

    adds a information if no product is registered

--- client/src/components/ProductsTable.tsx
+++ client/src/components/ProductsTable.tsx
@@ -5,18 +5,24 @@ export function ProductsTable() {
   const { products } = useProductContext();
 
   return (
-    <table className="w-full mt-10 mb-5">
-      <tr>
-        <th>Id</th>
-        <th>Image</th>
-        <th>Name</th>
-        <th>Price</th>
-        <th className="max-[1000px]:hidden">Stock</th>
-        <th>Actions</th>
-      </tr>
-      {products.map((product) => {
-        return <ProductRow product={product} key={product.id} />;
-      })}
-    </table>
+    <>
+      <table className="w-full mt-10 mb-5">
+        <tr>
+          <th>Id</th>
+          <th>Image</th>
+          <th>Name</th>
+          <th>Price</th>
+          <th className="max-[1000px]:hidden">Stock</th>
+          <th>Actions</th>
+        </tr>
+        {products.map((product) => {
+          return <ProductRow product={product} key={product.id} />;
+        })}
+      </table>
+
+      {!products.length && (
+        <p className="m-20 text-xl font-light">No product registered</p>
+      )}
+    </>
   );
 }

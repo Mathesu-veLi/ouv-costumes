169941e7257181722f5ff95369ebff9cbffb5916
Matheus <matheuslevit@gmail.com>
2025-01-24 18:04:24 -0300

    authorize access to edit product page

--- client/src/containers/EditProduct.tsx
+++ client/src/containers/EditProduct.tsx
@@ -1,5 +1,50 @@
+import { Loading } from '@/components/Loading';
+import { IProduct } from '@/interfaces/IProduct';
+import { useCartStore } from '@/store/useCartStore';
+import { useUserStore } from '@/store/useUserStore';
+import { authorizeAdmin, logout } from '@/utils/authorizationFunctions';
+import { fetchProduct } from '@/utils/productsFunctions';
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
 export function EditProduct() {
-    return <div className="w-full h-screen flex flex-col justify-center items-center text-center">
-        <h1>Product</h1>
+  const { id } = useParams();
+
+  const [product, setProduct] = useState<IProduct>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [authorized, setAuthorized] = useState(false);
+
+  const { token } = useUserStore();
+  const userStoreReset = useUserStore().reset;
+  const cartStoreReset = useCartStore().reset;
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function authorize() {
+      const authorized = await authorizeAdmin(token);
+
+      if (!authorized) {
+        return navigate('/');
+      }
+      if (authorized === 498) {
+        logout(userStoreReset, cartStoreReset);
+        return navigate('/login');
+      }
+
+      setAuthorized(authorized);
+    }
+
+    !authorized && authorize();
+  }, []);
+
+  if (isLoading) return <Loading />;
+
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center text-center">
+      <h1>Edit Product {id}</h1>
+
+      <button>Save</button>
     </div>
+  );
 }

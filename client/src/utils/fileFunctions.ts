e5f199fca3716a23827801ef5303aa64692178fc
Matheus <matheuslevit@gmail.com>
2025-01-25 01:36:25 -0300

    adds a check to verify that the image path is not undefined

--- client/src/utils/fileFunctions.ts
+++ client/src/utils/fileFunctions.ts
@@ -1,4 +1,5 @@
 export async function createFileObjectFromImage(imagePath: string) {
+  if (!imagePath) return;
   const blob = await (await fetch(imagePath)).blob();
   const imageFile = new File([blob], 'product.jpg', { type: blob.type });
   return imageFile;

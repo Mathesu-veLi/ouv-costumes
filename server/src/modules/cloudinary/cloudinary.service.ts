a3f8c8ea04e908f115f1eeed4bfb064f77d56c68
Matheus Levi <matheuslevit@gmail.com>
2025-09-29 11:14:32 -0300

    configure cloudinary

---
+++ server/src/modules/cloudinary/cloudinary.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { v2 as cloudinary, UploadApiResponse } from 'cloudinary';
+import { createReadStream } from 'streamifier';
+@Injectable()
+export class CloudinaryService {
+  async uploadImage(file: Express.Multer.File): Promise<UploadApiResponse> {
+    return new Promise((resolve, reject) => {
+      const uploadStream = cloudinary.uploader.upload_stream(
+        { folder: 'uploads' },
+        (error, result) => {
+          if (error) return reject(error);
+          resolve(result);
+        },
+      );
+
+      createReadStream(file.buffer).pipe(uploadStream);
+    });
+  }
+
+  async deleteImage(publicId: string) {
+    return cloudinary.uploader.destroy(publicId);
+  }
+}
